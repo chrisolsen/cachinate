c7c91e1a81aaa18777f5f887b515c265b5300b29
Chris Olsen <olsen.chris@gmail.com>
2009-10-01 14:55:32 -0600

    initial commit

---
+++ jquery.gs_cachinate.js
@@ -0,0 +1,79 @@
+/**
+ * gsCachinate
+ *
+ * Summary:
+ *    Cross between pagination and caching. On load an ajax request
+ *    will be made to fetch the next chunk of data.  If data is obtained
+ *    a link will be added below the table that is acting as the data 
+ *    containing element. Once the data is added to the table an additional
+ *    request will be made to retrieve the data for the next page.
+ *
+ * Created By:
+ *    Chris Olsen
+ */
+
+(function($) {
+
+  var gsCachinateOptions
+
+  $.fn.gsCachinate = function(options) {
+    
+    var defaultOptions = {
+      noDataMessage: "No More Data Exists",
+      nextPageLinkText: "View More",
+      queryStringKey: "page",
+    }
+    
+    options = options == null ? {} : options
+    gsCachinateOptions = $.extend(defaultOptions, options)
+
+    return this.each(function() {
+      getNextPage(1, $(this))
+    })
+  }
+
+  /**
+   * Recursive method that caches the next page of data and 
+   * adds it to the specified element when the user chooses
+   * to view more
+   */
+  function getNextPage(currentPage, dataElement) {
+    var urlBase = document.location.href + "?"+gsCachinateOptions["queryStringKey"]+"="
+
+    $.get(urlBase + (currentPage + 1), function(data) {
+      
+      var viewMoreId = 'gs-view-more'
+      var link
+
+      // Show or hide the data bases on whether more data exists
+      if (data.trim().length == 0) {
+        $("a#" + viewMoreId).remove()
+        dataElement.after("<div id='gs-no-data'>"+gsCachinateOptions["noDataMessage"]+"</div>")
+      }
+      else {
+        // create or obtain the existing link to view the next page
+        if ($("a#" + viewMoreId).length == 0) {
+          link = $("<a href='#'>")
+            .attr("id", viewMoreId)
+            .text(gsCachinateOptions["nextPageLinkText"])
+            .insertAfter(dataElement)
+        }
+        else {
+          link = $("a#" + viewMoreId)
+        }
+
+        // bind the unique attributes
+        link
+          .attr("href", urlBase + (currentPage+1))
+          .one("click", function() {
+            dataElement.find("tbody").append(data) 
+            getNextPage(currentPage + 1, dataElement)
+
+            return false
+          })
+
+      } // if
+    }) // get
+  } // function
+
+})(jQuery);
